5453753f0e3ccf58a946daa59aad79f7d300bf05
TS <sendy2012@naver.com>
2023-04-19 02:50:46 +0900

    수정3

--- src/main/java/kr/co/jhta/controller/UserController.java
+++ src/main/java/kr/co/jhta/controller/UserController.java
@@ -60,7 +60,7 @@ public class UserController {
     
     
     //회원가입 mn_auth1단계 인증코드
-   	@RequestMapping("login/mailConfirm")
+   	@PostMapping("login/mailConfirm")
     @ResponseBody
     String mailConfirm(@RequestParam("email") String email) throws Exception {
 
